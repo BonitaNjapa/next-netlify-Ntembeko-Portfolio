00b6c65419d1f544e2582349b517491a45536088
ntembeko.njapa <Ntembeko.njapa@xetgroup.com>
2022-01-12 01:24:09 +0200

    Change main page message

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Hi there I am happy you came to visit me!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Sorry but I am still under construction, please do come visit me again
+          soon :)
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
