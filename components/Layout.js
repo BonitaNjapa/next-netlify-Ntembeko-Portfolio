cabcef3d5851078069b41e1bf3dadc4bcc427e73
ntembeko.njapa <Ntembeko.njapa@xetgroup.com>
2022-02-06 17:01:05 +0200

    misc

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Footer from "./Footer";
+import NavBar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
     <>
+      <NavBar />
       <div className="container">
         <Head>
           <title>Hi there I am happy you came to visit me!</title>
---
+++ pages/cars/index.js
@@ -0,0 +1,9 @@
+const cars = () => {
+  return (
+    <>
+      <p className="description">All the list of cars</p>
+    </>
+  );
+};
+
+export default cars;
